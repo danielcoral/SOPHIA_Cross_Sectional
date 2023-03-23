120706b3518e494920cee4b432bb2730698b2b0d
FemkeSmit <48092836+FemkeSmit@users.noreply.github.com>
2023-03-23 18:46:46 +0100

    Function for getting cluster summary stats
    Returns summary stats in the form of a long dataframe (1 row for every cluster-variable combination)
    
    Includes:
    - number of people allocated with over 80% certainty 
    - percentage of people allocated with over 80% certainty
    - weighted mean for each variable
    - weighted standard deviation for each variable
    - weighted IQR for each variable
    
    Requires stat_dat and cluster_probs items for input.

---
+++ get_cluster_descriptives.R
@@ -0,0 +1,20 @@
+get_cluster_descriptives <- function(dataset, cluster_probs){
+  long_summary <- data.frame(cluster = integer(0), variable = character(0), n_allocated = integer(0), perc_allocated = numeric(0), wtd_mean = numeric(0), wtd_std = numeric(0), wtd_q0 = numeric(0), wtd_q25 = numeric(0), wtd_q50 = numeric(0), wtd_q70 = numeric(0), wtd_q100 = numeric(0))
+  for (col in 1:ncol(cluster_probs)){
+    clus <- colnames(cluster_probs)[col]
+    for (var in 1:ncol(dataset)){
+      varia <- colnames(dataset)[var]
+      n_allocated <- sum(cluster_probs[,col] > 0.8)
+      perc_allocated <- n_allocated/nrow(dataset)
+      wtd_mean <- wtd.mean(dataset[,var], cluster_probs[,col])
+      wtd_std <- wtd.var(dataset[,var], cluster_probs[,col]) %>% sqrt()
+      wtd_iqr <- wtd.quantile(dataset[,var], cluster_probs[,col])
+      row <- nrow(long_summary) + 1
+      long_summary[row,] <- c(clus, varia, n_allocated, perc_allocated, wtd_mean, wtd_std, wtd_iqr)
+    }
+  }
+  long_summary
+}
+
+# use the function like this: cluster_descriptives <- map2(strat_dat, cluster_probs, get_cluster_descriptives)
+# requires Hmisc package
